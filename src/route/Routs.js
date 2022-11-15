93b717f409313875594b75289be3a42fc3251359
takdirhossain <takdirhossain35@gmail.com>
2022-11-16 01:18:37 +0600

    added singup

--- src/route/Routs.js
+++ src/route/Routs.js
@@ -3,13 +3,15 @@ import Main from "../layout/Main";
 import GetAppoinment from "../pages/GetAppoinment";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Singup from "../pages/Singup";
 
 const route = createBrowserRouter([
     {
         path: '/', element: <Main></Main>, children: [
             { path: '/', element: <Home></Home> },
             {path: '/getappoinment', element: <GetAppoinment></GetAppoinment> },
-            {path: '/login', element: <Login></Login>}
+            {path: '/login', element: <Login></Login>},
+            {path: '/singup', element: <Singup></Singup>},
         ]
     }
 ])
